cf9c22d45f66730016fbb7178441e4b2d7216fcd
dra2a <63828961+dra2a@users.noreply.github.com>
2022-05-25 04:00:11 -0400

    updated tools styling

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -16,49 +16,52 @@ class Tools extends React.Component {
 
                 <div className="tools-container">
                     <div>
-                        <div className="tools-bg"> </div>
-                        <a href="https://cloud.google.com/">
-                            <img src={gcloud} alt='Google Cloud' className='tools-img'
-                                style={{ width: '170px', marginTop: '-160px', marginLeft: '-85px' }} />
-                        </a>
-                        <p className="tools-txt">Google Cloud</p>
+                        <div className="tools-item">
+                            <a href="https://cloud.google.com/">
+                                <img src={gcloud} alt='Google Cloud' className='tools-img' />
+                            </a>
+                            <p className="tools-txt">Google Cloud</p>
+                        </div>
                     </div>
                     <div>
-                        <div className="tools-bg"> </div>
-                        <a href="https://www.atlassian.com/software/jira">
-                            <img src={jira} alt='Jira' className='tools-img' />
-                        </a>
-                        <p className="tools-txt">Jira</p>
+                        <div className="tools-item">
+                            <a href="https://www.atlassian.com/software/jira">
+                                <img src={jira} alt='Jira' className='tools-img' />
+                            </a>
+                            <p className="tools-txt">Jira</p>
+                        </div>
                     </div>
                     <div>
-                        <div className="tools-bg"> </div>
-                        <a href="https://www.elastic.co/kibana/">
-                            <img src={kibana} alt='Kibana' className='tools-img'
-                                style={{ marginLeft: '-71px' }} />
-                        </a>
-                        <p className="tools-txt">Kibana</p>
+                        <div className="tools-item">
+                            <a href="https://www.elastic.co/kibana/">
+                                <img src={kibana} alt='Kibana' className='tools-img' />
+                            </a>
+                            <p className="tools-txt">Kibana</p>
+                        </div>
                     </div>
                     <div>
-                        <div className="tools-bg"> </div>
-                        <a href="https://www.mongodb.com/">
-                            <img src={mongo} alt='MongoDB' className='tools-img'
-                                style={{ width: '145px', marginTop: '-170px', marginLeft: '-73px' }} />
-                        </a>
-                        <p className="tools-txt">MongoDB</p>
+                        <div className="tools-item">
+                            <a href="https://www.mongodb.com/">
+                                <img src={mongo} alt='MongoDB' className='tools-img' />
+                            </a>
+                            <p className="tools-txt">MongoDB</p>
+                        </div>
                     </div>
                     <div>
-                        <div className="tools-bg"> </div>
-                        <a href="https://www.postman.com/">
-                            <img src={postman} alt='Postman' className='tools-img' />
-                        </a>
-                        <p className="tools-txt">Postman</p>
+                        <div className="tools-item">
+                            <a href="https://www.postman.com/">
+                                <img src={postman} alt='Postman' className='tools-img' />
+                            </a>
+                            <p className="tools-txt">Postman</p>
+                        </div>
                     </div>
                     <div>
-                        <div className="tools-bg"> </div>
-                        <a href="https://visualstudio.microsoft.com/">
-                        <img src={vs} alt='Visual Studio' className='tools-img' />
-                        </a>
-                        <p className="tools-txt">Visual Studio</p>
+                        <div className="tools-item">
+                            <a href="https://visualstudio.microsoft.com/">
+                                <img src={vs} alt='Visual Studio' className='tools-img' />
+                            </a>
+                            <p className="tools-txt">Visual Studio</p>
+                        </div>
                     </div>
                 </div>
             </div>
