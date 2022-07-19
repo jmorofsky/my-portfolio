f3c7782e80ac001966b64efa081a371c92da2334
Jason Morofsky <jmorofsky@gmail.com>
2022-07-18 22:05:32 -0400

    added new cert

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../css/Awards.css'
 import TitleBlock from './TitleBlock'
 import cert from '../images/AgileCertification.PNG'
+import cert2 from '../images/PostmanCertification.png'
 
 class Awards extends React.Component {
     render() {
@@ -13,8 +14,40 @@ class Awards extends React.Component {
                         <img src={cert} alt='My agile certification' />
                         <p className='awards-caption'>Agile with Atlassian Jira</p>
                     </div>
-                    <p className='awards-txt'>For this certification, I had to master best agile practices as well as
+                    <p className='awards-txt'>For this certifcate, I had to master best agile practices as well as
                         specific topics relating to Jira. I displayed my proficiency in JQL, Kanban, and Scrum.</p>
+                </div>
+
+                <div className='awards-container'>
+                    <div className='awardsImg-container'>
+                        <img src={cert2} alt='My agile certification' />
+                        <p className='awards-caption'>Postman: The Complete Guide</p>
+                    </div>
+                    <p className='awards-txt'>For this certifcate, utilizing Postman, I:
+                        <ul>
+                            <li className='list'>
+                                Created GET, POST, PUT, and DELETE requests
+                            </li>
+                            <li className='list'>
+                                Demonstrated my understanding of GET vs POST request methods
+                            </li>
+                            <li className='list'>
+                                Worked with real-world APIs (Github API, Trello API)
+                            </li>
+                            <li className='list'>
+                                Wrote API tests
+                            </li>
+                            <li className='list'>
+                                Used Postman variables to create workflows and scenarios
+                            </li>
+                            <li className='list'>
+                                Used OAuth2, API keys, tokens, JWT, and basic auth
+                            </li>
+                            <li className='list'>
+                                Collaborated using team workspaces
+                            </li>
+                        </ul>
+                    </p>
                 </div>
             </div>
         )
